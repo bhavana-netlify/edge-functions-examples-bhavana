a776a44bc79bb2cbcb95c1f38e55011f410568c7
bhavana <bhavana@netlify.com>
2022-05-09 15:33:17 -0400

    cleaning up split testing logic

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -7,9 +7,9 @@ export default async (request: Request, context: Context) => {
 
   // return here if we find a cookie
   if (bucket) {
-    //return new Response(`Welcome back! You were assigned ${bucketName} **${bucket}** when you last visited the site!`);
+    return new Response(`Welcome back! You were assigned ${bucketName} **${bucket}** when you last visited the site!`);
     //return context.rewrite("/something-to-serve-with-a-rewrite-A");
-    return context.json({ splittest: "versiona" });
+    //return context.json({ splittest: "versiona" });
   }
 
   // if no "test_bucket" cookie is found, assign the user to a bucket
@@ -31,8 +31,14 @@ export default async (request: Request, context: Context) => {
     //`Congratulations! You have been assigned ${bucketName} **${newBucketValue}**. View your browser cookies to check it out!`,
     
   //);
+  if (newBucketValue == "b") {
+    return context.json({ splittest: "versionb" });
+  }
 
-  return context.json({ splittest: "versionb" });
+  else{
+    return context.json({ splittest: "versiona" });
+  }
+
 
   /*  const joke = await fetch("https://icanhazdadjoke.com/", {
       "headers": {
