076c61231bbffe8553418dcfa9ea4d4704d049d2
bhavana <bhavana@netlify.com>
2022-05-09 11:45:54 -0400

    adding external URL

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -31,5 +31,15 @@ export default async (request: Request, context: Context) => {
     
   //);
 
-  return context.rewrite("https://www.google.com");
+  
+
+    const joke = await fetch("https://icanhazdadjoke.com/", {
+      "headers": {
+        "Accept": "application/json"
+      }
+    });
+    const jsonData = await joke.json();
+    return context.json(jsonData);
+  
+  
 };
